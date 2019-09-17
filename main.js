877fb8f05ba357f610edc2c01b284a405acc9646
Ryan Brockley <rybrockers@yahoo.com>
2019-09-17 11:29:29 +0100

    Completed the quiz

--- main.js
+++ main.js
@@ -6,18 +6,54 @@ const result = document.querySelector('.result');
 form.addEventListener('submit', event => {
     event.preventDefault();
 
-    
+
     let score = 0;
     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
-// check answers
-userAnswers.forEach((answer, index) => {
-    if(answer === correctAnswers[index]) {
-        score += 25;
-    }
-});
-// show results on page
-result.querySelector('span').textContent = `${score}%`;
-result.classList.remove('d-none');
+    // check answers
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+            score += 25;
+        }
+    });
+    // show results on page
+    scrollTo(0, 0);
+    result.querySelector('span').textContent = `${score}%`;
+    result.classList.remove('d-none');
+
+    let output = 0;
+
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score) {
+            clearInterval(timer);
+        } else {
+            output ++;
+        }
+    }, 10);
+
 });
 
+
+// window object (global)
+
+// console.log('hello');
+// window.console.log('hello');
+
+// console.log(document.querySelector('form'));
+// console.log(window.document.querySelector('form'));
+
+// alert('hello');
+// window.alert('hello');
+
+
+// let i = 0;
+
+// const timer = setInterval(() => {
+//     console.log('Hello');
+//     i++;
+//     if (i === 5) {
+//         clearInterval(timer);
+//     }
+
+// }, 1000);
